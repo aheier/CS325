9fe784c4e07cad6ab37ba6ce3b64bc2b235713c7
Anthony Heier Laptop <anthony.heier78@gmail.com>
2022-04-04 12:03:19 -0500

    04APRIL22

---
+++ Lab5/src/BinaryTreeApp.java
@@ -0,0 +1,258 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(int d) {
+		this.data = d;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class BinarySearchTree {
+	static final int COUNT = 10;
+	Node root;
+
+	BinarySearchTree() {
+		this.root = null;
+	}
+
+	/*
+	 * Task 1 - (2 points) Perform a BFS (Breadth first search) on a binary search
+	 * tree Print all the nodes (including root) visited during the search
+	 */
+	void BFS(int data) {
+		if (root == null) {
+			return;
+		}
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		Node current;
+		while (!queue.isEmpty()) {
+			current = queue.remove();
+			System.out.println("Search: " + current.data);
+			if (current.data == data) {
+				System.out.println("Found " + current.data);
+				return;
+			}
+			if (current.left != null) {
+				queue.add(current.left);
+			}
+			if (current.right != null) {
+				queue.add(current.right);
+			}
+
+		}
+		System.out.println("Not Found");
+	}
+
+	/*
+	 * Task 2 - (2 points) Perform a DFS (Depth first search) on a binary search
+	 * tree Print all the nodes (including root) visited during the search
+	 */
+	void DFS(int data) {
+		if (root == null) {
+			return;
+		}
+		Stack<Node> stack = new Stack<Node>();
+		stack.add(root);
+		Node current;
+		while (!stack.isEmpty()) {
+			current = stack.pop();
+			System.out.println("Search: " + current.data);
+			if (current.data == data) {
+				System.out.println("Found " + current.data);
+				return;
+			}
+			if (current.right != null) {
+				stack.add(current.right);
+			}
+			if (current.left != null) {
+				stack.add(current.left);
+			}
+
+		}
+		System.out.println("Not Found");
+	}
+
+	/*
+	 * Task 3 - (2 points) Remove a node based on the data passed. Print the 2D tree
+	 * after the removal.
+	 */
+	void removeNode(int data) {
+		if (root == null) {
+			return;
+		}
+		Node current = root;
+		Node prev = current;
+		while (current != null) {
+			System.out.println("Search: " + current.data);
+			if (current.data == data) {
+				// delete case
+//				System.out.println("Found " + current.data + " prev " + prev.data);
+				// leaf case
+				if (current.left == null) {
+					// leaf case
+					if (current.right == null) {
+						if (prev.data > current.data) {
+							prev.left = null;
+							return;
+						}
+						prev.right = null;
+						return;
+					}
+					// one to left case;
+					if (prev.data > current.data) {
+						prev.left = current.right;
+						return;
+					}
+					prev.right = current.right;
+					return;
+				}
+				if (current.right == null) {
+					// one to right case;
+					if (prev.data > current.data) {
+						prev.left = current.left;
+						return;
+					}
+					prev.right = current.left;
+					return;
+				}
+				//case where has 2 children
+				//TODO:
+				/*  Copy the contents of the inorder successor of the node to be deleted and delete the inorder successor. 
+				 *  The inorder successor can be found by finding the minimum element in the right subtree of the node. 
+				 */
+				
+				return;
+			}
+			if (current.data > data) {
+				prev = current;
+				current = current.left;
+				continue;
+			}
+			prev = current;
+			current = current.right;
+		}
+		System.out.println("Not Found");
+
+	}
+
+	/*
+	 * Task 4 - (2 points) Check if the binary search tree is a full binary tree or
+	 * not. If it is then prints "Full Binary Tree", otherwise
+	 * "Not a full binary tree". A full Binary tree is a special type of binary tree
+	 * in which every parent node has either two or no children.
+	 */
+	void isFullBinaryTree(int data) {
+	}
+
+	void addNode(int data) {
+		Node newNode = new Node(data);
+		if (root == null) {
+			root = newNode;
+			return;
+		}
+
+		Node current = root;
+		Node prev = null;
+		while (current != null) {
+			prev = current;
+			if (data < current.data) {
+				current = current.left;
+				continue;
+			}
+			current = current.right;
+		}
+
+		if (data < prev.data) {
+			prev.left = newNode;
+			return;
+		}
+		prev.right = newNode;
+	}
+
+	// Function to perform inOrder traversal on the tree using recursion
+	public void PrintInorderRecursive(Node root) {
+		if (root == null) {
+			return;
+		}
+		PrintInorderRecursive(root.left);
+		System.out.print(root.data + " ");
+		PrintInorderRecursive(root.right);
+	}
+
+	// Function to perform inOrder traversal using iterative approach
+	void PrintInOrder(Node root) {
+		if (root == null) {
+			return;
+		}
+		Stack<Node> s = new Stack<Node>();
+		Node current = root;
+		while (current != null || s.size() > 0) {
+			while (current != null) {
+				s.push(current);
+				current = current.left;
+			}
+			current = s.pop();
+			System.out.print(current.data + " ");
+			current = current.right;
+		}
+	}
+
+	/* This prints the binary tree in 2D fashion */
+	void print2DTree(Node root, int space) {
+		if (root == null)
+			return;
+
+		space += COUNT;
+
+		print2DTree(root.right, space);
+		System.out.print("\n");
+		for (int i = COUNT; i < space; i++) {
+			System.out.print(" ");
+		}
+		System.out.print(root.data + "\n");
+
+		print2DTree(root.left, space);
+	}
+}
+
+public class BinaryTreeApp {
+	public static void main(String[] args) {
+		BinarySearchTree b = new BinarySearchTree();
+
+		b.addNode(10);
+		b.addNode(4);
+		b.addNode(3);
+		b.addNode(5);
+		b.addNode(15);
+		b.addNode(12);
+		b.addNode(11);
+		b.addNode(6);
+		b.addNode(17);
+		b.PrintInOrder(b.root);
+		b.print2DTree(b.root, 0);
+
+//		System.out.println("Breadth");
+//		b.BFS(11);
+//		b.BFS(1);
+//		b.BFS(15);
+
+//		System.out.println("Depth");
+//		b.DFS(11);
+//		b.DFS(1);
+//		b.DFS(15);
+
+		b.removeNode(15);
+//		b.removeNode(6);
+//		b.removeNode(11);
+		b.PrintInOrder(b.root);
+		b.print2DTree(b.root, 0);
+	}
+}
---
+++ Lab5/src/HuffmanDecode.java
@@ -0,0 +1,35 @@
+import java.util.*; 
+
+public class HuffmanDecode {
+	public static void main(String[] args) {
+		/* Java Hash table is like a Python dictionary 
+		 * where there is a key and value. Keys must be unique.
+		 */
+		Hashtable<String, Character> my_dict = new Hashtable<String, Character>();
+		
+		/* Adding items into the dictionary/hash table
+		 * following is the mapping after Huffman coding algorithm is applied 
+		 */
+		my_dict.put("0000", 'o');
+		my_dict.put("0001", 'x');
+		my_dict.put("0010", 'i');
+		my_dict.put("0011", 'b');
+		my_dict.put("01", 'a');
+		my_dict.put("11", 'n');
+		my_dict.put("10", ' ');
+		
+		/* Example of how to print all the keys and values */
+		Set<String> keys = my_dict.keySet();
+        for(String key: keys){
+            System.out.println("Value of "+key+" is: "+my_dict.get(key));
+        }
+		
+        /* Example of how to find 1 item using the key */
+        System.out.println("Value is: "+my_dict.get("0010"));  // This prints 'i'
+        
+		/* Following is the code for a specific text based on the dictionary */
+		String code = "0011011101110110001011100110001100000001";
+		
+		/* Task: Figure out the original text */
+	}
+}
